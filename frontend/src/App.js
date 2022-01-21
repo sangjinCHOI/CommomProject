009187b65752fe2143ec6c20699b6af8eababc70
DESKTOP-TMRS7FM\pkonu <pjhq2@naver.com>
2022-01-21 19:30:48 +0900

    feat: React App 생성

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "@material-tailwind/react/tailwind.css";
+import Button from "@material-tailwind/react/Button";
+
+function App() {
+  return (
+    <div>
+      <Button>frontend</Button>
+    </div>
+  );
+}
+
+export default App;
