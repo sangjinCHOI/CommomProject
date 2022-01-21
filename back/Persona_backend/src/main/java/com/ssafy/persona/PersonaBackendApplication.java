ae4d67a19b6b4558e90e3c3c78d54da24819e65f
fbeotjd <fb1014@naver.com>
2022-01-21 15:51:37 +0900

    feat: back project

---
+++ back/Persona_backend/src/main/java/com/ssafy/persona/PersonaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.persona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaBackendApplication.class, args);
+	}
+
+}
