6272718c02237348eb22f831def6a57f8813ce6a
최대치 <eocl777@naver.com>
2022-01-21 21:39:53 +0900

    Design: button 디자인 테스트

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,19 @@ function App() {
   return (
     <div>
       <Button>frontend</Button>
+
+      <div class="flex flex-wrap items-end gap-x-2 gap-y-8">
+        <button class="false flex items-center justify-center gap-1 font-bold outline-none uppercase tracking-wider focus:outline-none focus:shadow-none transition-all duration-300 rounded-lg py-1.5 px-4 text-xs leading-normal text-white bg-red-500 hover:bg-light-blue-700 focus:bg-light-blue-400 active:bg-light-blue-800 shadow-md-light-blue hover:shadow-lg-light-blue undefined">
+          small_test
+        </button>
+        <button class="false flex items-center justify-center gap-1 font-bold outline-none uppercase tracking-wider focus:outline-none focus:shadow-none transition-all duration-300 rounded-lg py-2.5 px-6 text-xs leading-normal text-white bg-light-blue-500 hover:bg-orange-700 focus:bg-light-blue-400 active:bg-light-blue-800 shadow-md-light-blue hover:shadow-lg-light-blue undefined">
+          regular_test
+        </button>
+        <button class="false flex items-center justify-center gap-1 font-bold outline-none uppercase tracking-wider focus:outline-none focus:shadow-none transition-all duration-300 rounded-lg py-3 px-7 text-sm leading-relaxed text-white bg-green-500 hover:bg-light-blue-700 focus:bg-blue-400 active:bg-light-blue-800 shadow-md-light-blue hover:shadow-lg-light-blue undefined">
+          large_test
+        </button>
+      </div>
+      
     </div>
   );
 }
