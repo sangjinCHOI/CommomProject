ceff9cf8e2dc4ac8c721b2e07dc91225e4ec803f
DESKTOP-TMRS7FM\pkonu <pjhq2@naver.com>
2022-01-24 14:12:41 +0900

    docs: assets/images 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "@material-tailwind/react/tailwind.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import Characters from "./router/Characters";
 import Home from "./router/Home";
 import Signup from "./router/Signup";
 
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           {/* Layout 필요 없는 주소 */}
           <Route path="/signup" component={Signup} />
+          <Route path="/characters" component={Characters} />
           {/* Layout 필요한 주소 */}
           <Layout>
             <Route exact path="/" component={Home} />
---
+++ frontend/src/router/Characters.js
@@ -0,0 +1,3 @@
+export default function Characters() {
+  return <div>characters</div>;
+}
