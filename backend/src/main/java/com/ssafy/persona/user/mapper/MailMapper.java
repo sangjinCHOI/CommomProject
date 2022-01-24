7a2a0612891ce21d79b0d4ac0a4e49e3113c525a
fbeotjd <fb1014@naver.com>
2022-01-24 11:51:31 +0900

    feat: builder 패턴 추가 패키지 구조 추가

---
+++ backend/src/main/java/com/ssafy/persona/user/mapper/MailMapper.java
@@ -0,0 +1,5 @@
+package com.ssafy.persona.user.mapper;
+
+public interface MailMapper {
+
+}
--- backend/src/main/java/com/ssafy/persona/user/model/entity/User.java
+++ backend/src/main/java/com/ssafy/persona/user/model/entity/User.java
@@ -1,27 +1,35 @@
-package com.ssafy.persona.user.model.dto;
+package com.ssafy.persona.user.model.entity;
 
 import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
 @AllArgsConstructor
-public class UserResponse {
+@RequiredArgsConstructor
+public class User {
+	@Id
 	private int userSeq;
+	@Column
 	private String userId;
+	@Column
 	private String userPw;
+	@Column
 	private String userEmail;
+	@Column
 	private boolean emailIsAuth;
+	@Column
 	private String userBirth;
+	@Column
 	private boolean userIsActive;
+	@Column
 	private LocalDateTime userCreatedDate;
+	@Column
 	private LocalDateTime userModifiedDate;
 	
 }
