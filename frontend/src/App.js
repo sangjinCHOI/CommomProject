ebf21434b02814d20e7c31d674c1655c4df4d09c
최대치 <eocl777@naver.com>
2022-01-24 22:39:45 +0900

    Feat : IdInquiryResult page 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Signup from "./router/Signup";
 import SignupEmail from "./router/SignupEmail";
 import Login from "./router/Login";
 import IdInquiry from "./router/IdInquiry";
+import IdInquiryResult from "./router/IdInquiryResult";
 import Profile from "./router/Profile";
 import Follow from "./router/Follow";
 
@@ -20,7 +21,8 @@ function App() {
           <Route exact path="/signup" component={Signup} />
           <Route path="/signup/email" component={SignupEmail} />
           <Route path="/login" component={Login} />
-          <Route path="/idinquiry" component={IdInquiry} />
+          <Route exact path="/idinquiry" component={IdInquiry} />
+          <Route path="/idinquiry/result" component={IdInquiryResult} />
           <Route exact path="/characters" component={Characters} />
           <Route exact path="/characters/create" component={CharactersCreate} />
           {/* Layout 필요한 주소 */}
