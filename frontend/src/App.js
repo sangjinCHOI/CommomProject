08944398a9e9919f9531b06bfa11af20e1f7a5f6
DESKTOP-TMRS7FM\pkonu <pjhq2@naver.com>
2022-01-24 17:49:59 +0900

    design: Characters, CharactersCreate 페이지 초기 디자인

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "@material-tailwind/react/tailwind.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Characters from "./router/Characters";
+import CharactersCreate from "./router/CharactersCreate";
 import Home from "./router/Home";
 import Signup from "./router/Signup";
 
@@ -12,7 +13,8 @@ function App() {
         <Switch>
           {/* Layout 필요 없는 주소 */}
           <Route path="/signup" component={Signup} />
-          <Route path="/characters" component={Characters} />
+          <Route exact path="/characters" component={Characters} />
+          <Route path="/characters/create" component={CharactersCreate} />
           {/* Layout 필요한 주소 */}
           <Layout>
             <Route exact path="/" component={Home} />
--- frontend/src/components/CharactersProfile.js
+++ frontend/src/components/CharactersProfile.js
@@ -3,19 +3,20 @@ import { Image } from "@material-tailwind/react";
 import styles from "./CharactersProfile.module.css";
 import defaultUserImg from "../assets/images/default_user_img.png";
 
-export default function ProfileImg() {
+export default function CharactersProfile({ nickname, updateUserImg }) {
   return (
     <div className="mx-16 my-8">
       <div className={`${styles.center}`}>
         <Image
           src={defaultUserImg}
-          alt="profile"
+          alt="user_img"
           width="100px"
           rounded={true}
           className={(`${styles.profileWrapper}`, `${styles.ProfileImg}`)}
         />
       </div>
-      <div className={`${styles.center}`}>nickname</div>
+      <div className={`${styles.center}`}>{nickname}</div>
+      <div className={`${styles.center}`}>{updateUserImg}</div>
     </div>
   );
 }
