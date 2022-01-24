140e1f8359c9c7c3801dfaaa84a8e909f6952a81
kimdy96 <kp03@naver.com>
2022-01-24 17:48:39 +0900

    Revert "feat: swaggerconfig 설정"
    This reverts commit 03d9780ae954a8030d70003dcf26733bc2d27e82.

--- backend/src/main/java/com/ssafy/persona/config/SwaggerConfig.java
+++ backend/src/main/java/com/ssafy/persona/config/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.OAS_30)
                 .useDefaultResponseMessages(false)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.ssafy.persona"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.springswagger.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
