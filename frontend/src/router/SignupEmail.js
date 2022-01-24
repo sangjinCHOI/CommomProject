f5f78fca09d4da8d52d225510700509b83e29a12
최대치 <eocl777@naver.com>
2022-01-24 22:06:59 +0900

    feat : SignupEmail page 구현

---
+++ frontend/src/router/SignupEmail.js
@@ -0,0 +1,43 @@
+import "@material-tailwind/react/tailwind.css";
+import CardBody from "@material-tailwind/react/CardBody";
+import CardFooter from "@material-tailwind/react/CardFooter";
+import InputIcon from "@material-tailwind/react/InputIcon";
+import Card from "@material-tailwind/react/Card";
+import Button from "@material-tailwind/react/Button";
+import Logo from "../assets/images/main_logo.png";
+import styles from "./Signup.module.css";
+
+export default function Signup() {
+  return (
+    <div className={`${styles.center}`}>
+      <div id="logo" className={`${styles.logo}`}>
+        <img src={Logo} />
+      </div>
+
+      <Card>
+        <CardBody>
+          <p align="center">가입하신 이메일로 인증메일이 발송되었습니다.</p>
+          <br />
+          <p align="center">메일함을 확인해주세요.</p>
+        </CardBody>
+      </Card>
+
+      <CardFooter>
+        <div className="flex justify-center">
+          <Button color="lightBlue" buttonType="link" size="lg" ripple="dark">
+            메일함 이동
+          </Button>
+          <Button color="lightBlue" buttonType="link" size="lg" ripple="dark">
+            메일 재발송
+          </Button>
+        </div>
+
+        <div>
+          <a className="flex justify-center" href="../Login">
+            Login
+          </a>
+        </div>
+      </CardFooter>
+    </div>
+  );
+}
