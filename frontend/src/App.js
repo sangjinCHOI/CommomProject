822ba701af439ab3bbdbd708bfecf2f9258f38b2
최상진 <toramx2@naver.com>
2022-01-25 07:37:08 +0900

    Feat: create settings page router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,12 @@ import Signup from "./router/Signup";
 import SignupEmail from "./router/SignupEmail";
 import Login from "./router/Login";
 import Alarm from "./router/Alarm"
+import SettingsCharacter from "./router/SettingsCharacter";
+import SettingsAccount from "./router/SettingsAccout";
+import SettingsAlarm from "./router/SettingsAlarm";
+import SettingsHelp from "./router/SettingsHelp";
 import Profile from "./router/Profile";
 import Follow from "./router/Follow";
-
 
 function App() {
   return (
@@ -26,6 +29,10 @@ function App() {
           {/* Layout 필요한 주소 */}
           <Layout>
             <Route exact path="/" component={Home} />
+            <Route exact path="/settings/character" component={SettingsCharacter} />
+            <Route exact path="/settings/account" component={SettingsAccount} />
+            <Route exact path="/settings/alarm" component={SettingsAlarm} />
+            <Route exact path="/settings/help" component={SettingsHelp} />
             <Route exact path="/alarm" component={Alarm} />
             {/* characters, login, signup같은 닉네임이 있다면 문제 발생 가능 주의 */}
             <Route exact path="/:nickname" component={Profile} />
