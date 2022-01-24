8c6f66f3dbc427f6dc97814a905f558215100c0f
DESKTOP-TMRS7FM\pkonu <pjhq2@naver.com>
2022-01-24 12:02:05 +0900

    feat: router, Navbar, Layout upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,23 @@
 import "@material-tailwind/react/tailwind.css";
-import Button from "@material-tailwind/react/Button";
-import Alert from "@material-tailwind/react/Alert";
-import ClosingAlert from "@material-tailwind/react/ClosingAlert";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./router/Home";
+import Signup from "./router/Signup";
 
 function App() {
   return (
-    <div>
-      <Button>frontend</Button>
-
-      <div class="flex flex-wrap items-end gap-x-2 gap-y-8">
-        <button class="false flex items-center justify-center gap-1 font-bold outline-none uppercase tracking-wider focus:outline-none focus:shadow-none transition-all duration-300 rounded-lg py-1.5 px-4 text-xs leading-normal text-white bg-red-500 hover:bg-light-blue-700 focus:bg-light-blue-400 active:bg-light-blue-800 shadow-md-light-blue hover:shadow-lg-light-blue undefined">
-          small_test
-        </button>
-        <button class="false flex items-center justify-center gap-1 font-bold outline-none uppercase tracking-wider focus:outline-none focus:shadow-none transition-all duration-300 rounded-lg py-2.5 px-6 text-xs leading-normal text-white bg-light-blue-500 hover:bg-orange-700 focus:bg-light-blue-400 active:bg-light-blue-800 shadow-md-light-blue hover:shadow-lg-light-blue undefined">
-          regular_test
-        </button>
-        <button class="false flex items-center justify-center gap-1 font-bold outline-none uppercase tracking-wider focus:outline-none focus:shadow-none transition-all duration-300 rounded-lg py-3 px-7 text-sm leading-relaxed text-white bg-green-500 hover:bg-light-blue-700 focus:bg-blue-400 active:bg-light-blue-800 shadow-md-light-blue hover:shadow-lg-light-blue undefined">
-          large_test
-        </button>
-      </div>
-      <br />
-      <Alert color="green">LOGIN SUCCESS</Alert>
-      <ClosingAlert color="red">LOGIN FAIL</ClosingAlert>
-      
-    </div>
+    <>
+      <Router>
+        <Switch>
+          {/* Layout 필요 없는 주소 */}
+          <Route path="/signup" component={Signup} />
+          {/* Layout 필요한 주소 */}
+          <Layout>
+            <Route exact path="/" component={Home} />
+          </Layout>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,3 @@
+export default function Navbar() {
+  return <div>네브바</div>;
+}
