4b4bbc18c95d7ca518e71e5836f158474b19c343
최상진 <toramx2@naver.com>
2022-01-25 10:02:48 +0900

    Feat: create settings page router

---
+++ frontend/src/router/SettingsAccout.js
@@ -0,0 +1,7 @@
+export default function SettingsAccount() {
+  return (
+    <div className="text-3xl">
+      <h1>setting account</h1>
+    </div>
+  );
+}
---
+++ frontend/src/router/SettingsAlarm.js
@@ -0,0 +1,7 @@
+export default function SettingsAlarm() {
+  return (
+    <div className="text-3xl">
+      <h1>setting alarm</h1>
+    </div>
+  );
+}
