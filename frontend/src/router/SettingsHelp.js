c4d3b27db1f20af137f9c3bd25fce3b61d7dfa2e
최상진 <toramx2@naver.com>
2022-01-26 01:48:35 +0900

    Design: settings help

--- frontend/src/router/SettingsHelp.js
+++ frontend/src/router/SettingsHelp.js
@@ -1,7 +1,53 @@
+import { Card, CardBody, CardHeader } from "@material-tailwind/react";
+
 export default function SettingsHelp() {
   return (
-    <div className="text-3xl">
-      <h1>setting help</h1>
-    </div>
+    <>
+      <div className="flex mx-10 mt-20">
+        <Card>
+          <CardHeader color="lightBlue" contentPosition="left">
+            <h2 className="text-white text-2xl">도움말 및 지원</h2>
+          </CardHeader>
+          <CardBody className="pt-0 mx-5">
+            <div className="overflow-x-auto">
+              <table className="items-center w-full bg-transparent border-collapse">
+                <tbody>
+                  <tr>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-10 py-4 text-left">
+                      <span className="text-lg font-bold">고객센터</span>
+                    </th>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left"></th>
+                  </tr>
+                  <tr>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-10 py-4 text-left">
+                      <span className="text-lg font-bold">커뮤니티 규정</span>
+                    </th>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left"></th>
+                  </tr>
+                  <tr>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-10 py-4 text-left">
+                      <span className="text-lg font-bold">데이터 정책</span>
+                    </th>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center"></th>
+                  </tr>
+                  <tr>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-10 py-4 text-left">
+                      <span className="text-lg font-bold">이용 약관</span>
+                    </th>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center"></th>
+                  </tr>
+                  <tr>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-10 py-4 text-left">
+                      <span className="text-lg font-bold">정보</span>
+                    </th>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center"></th>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </CardBody>
+        </Card>
+      </div>
+    </>
   );
 }
