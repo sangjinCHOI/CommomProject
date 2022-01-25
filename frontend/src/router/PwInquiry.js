84d6196798c49a3bbf1f2235997f9fb8b5bca1e5
최대치 <eocl777@naver.com>
2022-01-25 14:47:31 +0900

    Design : Input 디자인 수정

--- frontend/src/router/PwInquiry.js
+++ frontend/src/router/PwInquiry.js
@@ -1,8 +1,6 @@
 import "@material-tailwind/react/tailwind.css";
-import CardBody from "@material-tailwind/react/CardBody";
 import CardFooter from "@material-tailwind/react/CardFooter";
 import InputIcon from "@material-tailwind/react/InputIcon";
-import Card from "@material-tailwind/react/Card";
 import Button from "@material-tailwind/react/Button";
 import Logo from "../assets/images/main_logo.png";
 import styles from "./Signup.module.css";
@@ -18,26 +16,24 @@ export default function PwInquiry() {
         <img src={Logo} />
       </div>
 
-      <Card>
-        <CardBody>
-          <p align="center">가입한 ID을 입력해 주세요.</p>
+      <p align="center">가입한 ID을 입력해 주세요.</p>
 
-          <div className="mt-3 mb-4 px-4">
-            <InputIcon
-              type="text"
-              color="lightBlue"
-              placeholder="ID"
-              outline={true}
-              iconName="person"
-              onChange={function () {
-                setEmailShow(true);
-              }}
-            />
-          </div>
+      <div className="mt-3 mb-4 px-11">
+        <div className="bg-white rounded-lg">
+          <InputIcon
+            type="text"
+            color="lightBlue"
+            placeholder="ID"
+            outline={true}
+            iconName="person"
+            onChange={function () {
+              setEmailShow(true);
+            }}
+          />
+        </div>
+      </div>
 
-          {emailShow ? <EmailComp></EmailComp> : null}
-        </CardBody>
-      </Card>
+      {emailShow ? <EmailComp></EmailComp> : null}
 
       <CardFooter>
         <div className="flex justify-center">
@@ -61,14 +57,16 @@ function EmailComp() {
     <>
       <p align="center">가입한 이메일을 입력해 주세요.</p>
 
-      <div className="mt-3 mb-5 px-4">
-        <InputIcon
-          type="text"
-          color="lightBlue"
-          placeholder="E-mail"
-          outline={true}
-          iconName="person"
-        />
+      <div className="mt-3 mb-5 px-11">
+        <div className="bg-white rounded-lg">
+          <InputIcon
+            type="text"
+            color="lightBlue"
+            placeholder="E-mail"
+            outline={true}
+            iconName="person"
+          />
+        </div>
       </div>
     </>
   );
