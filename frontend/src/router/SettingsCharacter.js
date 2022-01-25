9f3e2c98a666953f10e325bca61ab198b21587b8
최상진 <toramx2@naver.com>
2022-01-25 15:23:35 +0900

    Design: Input 디자인 수정

--- frontend/src/router/SettingsCharacter.js
+++ frontend/src/router/SettingsCharacter.js
@@ -10,23 +10,29 @@ export default function SettingsCharacter() {
           <span>캐릭터 삭제</span>
         </div>
         <div className="w-96 mx-auto mt-16 flex flex-col justify-center">
-          <InputIcon
-            placeholder={"NICKNAME"}
-            outline={true}
-            color={"blueGray"}
-          />
+          <div className="bg-white rounded-lg">
+            <InputIcon
+              placeholder={"NICKNAME"}
+              outline={true}
+              color={"blueGray"}
+            />
+          </div>
           <input
             type="text"
             value="요리"
             disabled
             class="my-3 block w-full px-3 py-2 border border-slate-300 rounded-md disabled:bg-slate-300 disabled:text-black-500"
           />
-          <Textarea
-            placeholder={"한 줄 소개를 입력하세요."}
-            outline={true}
-            color={"blueGray"}
-            className="mt-8"
-          />
+          <div
+            className="bg-white rounded-md rounded-lg"
+            style={{ height: 185 }}
+          >
+            <Textarea
+              placeholder={"한 줄 소개를 입력하세요."}
+              outline={true}
+              color={"blueGray"}
+            />
+          </div>
           <Button className="my-3">저장</Button>
         </div>
       </div>
--- frontend/src/router/Signup.js
+++ frontend/src/router/Signup.js
@@ -1,8 +1,6 @@
 import "@material-tailwind/react/tailwind.css";
-import CardBody from "@material-tailwind/react/CardBody";
 import CardFooter from "@material-tailwind/react/CardFooter";
 import InputIcon from "@material-tailwind/react/InputIcon";
-import Card from "@material-tailwind/react/Card";
 import Button from "@material-tailwind/react/Button";
 import Logo from "../assets/images/main_logo.png";
 import styles from "./Signup.module.css";
@@ -14,52 +12,57 @@ export default function Signup() {
       <div id="logo" className={`${styles.logo}`}>
         <img src={Logo} />
       </div>
+      <br />
 
-      <Card>
-        <CardBody>
-          {/* <Input type="email" placeholder="Full Name" color="lightBlue" outline={true} /> */}
+      {/* <Input type="email" placeholder="Full Name" color="lightBlue" outline={true} /> */}
 
-          <div className="mt-3 mb-5 px-4">
-            <InputIcon
-              type="text"
-              color="lightBlue"
-              placeholder="USER ID"
-              outline={true}
-              iconName="person"
-            />
-          </div>
+      <div className="mt-3 mb-5 px-11">
+        <div className="bg-white rounded-lg">
+          <InputIcon
+            type="text"
+            color="lightBlue"
+            placeholder="USER ID"
+            outline={true}
+            iconName="person"
+          />
+        </div>
+      </div>
 
-          <div className="mb-5 px-4">
-            <InputIcon
-              type="password"
-              color="lightBlue"
-              placeholder="Password"
-              outline={true}
-              iconName="pin"
-            />
-          </div>
+      <div className="mb-5 px-11">
+        <div className="bg-white rounded-lg">
+          <InputIcon
+            type="password"
+            color="lightBlue"
+            placeholder="Password"
+            outline={true}
+            iconName="pin"
+          />
+        </div>
+      </div>
 
-          <div className="mb-5 px-4">
-            <InputIcon
-              type="password"
-              color="lightBlue"
-              placeholder="Password Check"
-              outline={true}
-              iconName="pin"
-            />
-          </div>
+      <div className="mb-5 px-11">
+        <div className="bg-white rounded-lg">
+          <InputIcon
+            type="password"
+            color="lightBlue"
+            placeholder="Password Check"
+            outline={true}
+            iconName="pin"
+          />
+        </div>
+      </div>
 
-          <div className="mb-4 px-4">
-            <InputIcon
-              type="email"
-              color="lightBlue"
-              placeholder="Email Address"
-              outline={true}
-              iconName="email"
-            />
-          </div>
-        </CardBody>
-      </Card>
+      <div className="mb-4 px-11">
+        <div className="bg-white rounded-lg">
+          <InputIcon
+            type="email"
+            color="lightBlue"
+            placeholder="Email Address"
+            outline={true}
+            iconName="email"
+          />
+        </div>
+      </div>
 
       <CardFooter>
         <div className="flex justify-center">
