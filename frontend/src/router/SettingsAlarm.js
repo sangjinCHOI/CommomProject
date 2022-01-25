4ffb60de9a8525e12f01c18dd16c782e21cbe3a4
최상진 <toramx2@naver.com>
2022-01-26 01:48:10 +0900

    Design: settings alarm

--- frontend/src/router/SettingsAlarm.js
+++ frontend/src/router/SettingsAlarm.js
@@ -1,7 +1,53 @@
+import { Card, CardBody, CardHeader } from "@material-tailwind/react";
+
 export default function SettingsAlarm() {
   return (
-    <div className="text-3xl">
-      <h1>setting alarm</h1>
-    </div>
+    <>
+      <div className="flex mx-10 mt-20">
+        <Card>
+          <CardHeader color="lightBlue" contentPosition="left">
+            <h2 className="text-white text-2xl">알림</h2>
+          </CardHeader>
+          <CardBody className="pt-0 mx-5">
+            <div className="overflow-x-auto">
+              <table className="items-center w-full bg-transparent border-collapse">
+                <tbody>
+                  <tr>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-10 py-4 text-left">
+                      <span className="text-lg font-bold">모든 알림 허용</span>
+                    </th>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left"></th>
+                  </tr>
+                  <tr>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-10 py-4 text-left">
+                      <span className="text-lg font-bold">좋아요 알림</span>
+                    </th>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left"></th>
+                  </tr>
+                  <tr>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-10 py-4 text-left">
+                      <span className="text-lg font-bold">댓글 알림</span>
+                    </th>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center"></th>
+                  </tr>
+                  <tr>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-10 py-4 text-left">
+                      <span className="text-lg font-bold">팔로우 알림</span>
+                    </th>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center"></th>
+                  </tr>
+                  <tr>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-10 py-4 text-left">
+                      <span className="text-lg font-bold">저장한 게시물 수정 및 삭제 알림</span>
+                    </th>
+                    <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center"></th>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </CardBody>
+        </Card>
+      </div>
+    </>
   );
 }
