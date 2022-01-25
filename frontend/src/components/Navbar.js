c3ef946bef823c20d0c7e60f190ed4192885bf21
최상진 <toramx2@naver.com>
2022-01-25 20:02:06 +0900

    Feat: navbar search

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { Disclosure, Menu } from "@headlessui/react";
 import Logo from "../assets/images/main_logo.png";
 import ShortLogo from "../assets/images/short_logo.png";
 import Search from "../assets/images/search.png";
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
-
 export default function Example() {
+  const [word, setWord] = React.useState("");
+  const history = useHistory();
+  const onChange = (event) => {
+    setWord(event.target.value);
+  };
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      history.push(`/search?query=${word}`);
+    }
+  };
+
   return (
     <div
       style={{
@@ -25,16 +32,8 @@ export default function Example() {
             <div className="flex items-center justify-between h-16">
               <div className="">
                 <Link to="/">
-                  <img
-                    className="block md:hidden h-10"
-                    src={ShortLogo}
-                    alt="logo"
-                  />
-                  <img
-                    className="hidden md:block h-10"
-                    src={Logo}
-                    alt="shortlogo"
-                  />
+                  <img className="block md:hidden h-10" src={ShortLogo} alt="logo" />
+                  <img className="hidden md:block h-10" src={Logo} alt="shortlogo" />
                 </Link>
               </div>
 
@@ -44,34 +43,26 @@ export default function Example() {
                   style={{ width: "300px" }}
                   type="text"
                   placeholder="search"
+                  value={word}
+                  onChange={onChange}
+                  onKeyPress={handleKeyPress}
                 />
-                <img
-                  className="absolute inset-y-2 right-3"
-                  src={Search}
-                  alt=""
-                />
+                <Link to={{ pathname: "/search", search: `?query=${word}` }}>
+                  <img className="absolute inset-y-2 right-3" src={Search} alt="" />
+                </Link>
               </div>
 
               <div className="flex items-center static inset-auto ml-6 pr-0">
-                <span
-                  className="hidden md:block material-icons h-10 w-10 mt-1 mx-2"
-                  style={{ fontSize: 40 }}
-                >
+                <span className="hidden md:block material-icons h-10 w-10 mt-1 mx-2" style={{ fontSize: 40 }}>
                   add_circle_outline
                 </span>
                 <Link to="/alarm">
-                  <span
-                    className="hidden md:block material-icons h-10 w-10 mt-1 mx-2"
-                    style={{ fontSize: 40 }}
-                  >
+                  <span className="hidden md:block material-icons h-10 w-10 mt-1 mx-2" style={{ fontSize: 40 }}>
                     notifications_none
                   </span>
                 </Link>
                 <Link to="">
-                  <span
-                    className="hidden md:block material-icons h-10 w-10 mt-1 mx-2"
-                    style={{ fontSize: 40 }}
-                  >
+                  <span className="hidden md:block material-icons h-10 w-10 mt-1 mx-2" style={{ fontSize: 40 }}>
                     person_outline
                   </span>
                 </Link>
@@ -79,60 +70,39 @@ export default function Example() {
                 <Menu as="div" className="mx-2 relative">
                   <div>
                     <Menu.Button className="flex text-sm">
-                      <span
-                        className="material-icons h-10 w-10 mx-2"
-                        style={{ fontSize: 40 }}
-                      >
+                      <span className="material-icons h-10 w-10 mx-2" style={{ fontSize: 40 }}>
                         menu
                       </span>
                     </Menu.Button>
                   </div>
                   <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <Menu.Item>
-                      <Link
-                        to="/"
-                        className="block md:hidden px-4 py-2 text-sm text-gray-700"
-                      >
+                      <Link to="/" className="block md:hidden px-4 py-2 text-sm text-gray-700">
                         게시글 작성
                       </Link>
                     </Menu.Item>
                     <Menu.Item>
-                      <Link
-                        to="/"
-                        className="block md:hidden px-4 py-2 text-sm text-gray-700"
-                      >
+                      <Link to="/" className="block md:hidden px-4 py-2 text-sm text-gray-700">
                         알림
                       </Link>
                     </Menu.Item>
                     <Menu.Item>
-                      <Link
-                        to="/"
-                        className="block md:hidden px-4 py-2 text-sm text-gray-700"
-                      >
+                      <Link to="/" className="block md:hidden px-4 py-2 text-sm text-gray-700">
                         프로필
                       </Link>
                     </Menu.Item>
                     <Menu.Item>
-                      <Link
-                        to="/"
-                        className="block px-4 py-2 text-sm text-gray-700"
-                      >
+                      <Link to="/" className="block px-4 py-2 text-sm text-gray-700">
                         캐릭터 변경
                       </Link>
                     </Menu.Item>
                     <Menu.Item>
-                      <Link
-                        to="/settings/character"
-                        className="block px-4 py-2 text-sm text-gray-700"
-                      >
+                      <Link to="/settings/character" className="block px-4 py-2 text-sm text-gray-700">
                         설정
                       </Link>
                     </Menu.Item>
                     <Menu.Item>
-                      <Link
-                        to="/"
-                        className="block px-4 py-2 text-sm text-gray-700"
-                      >
+                      <Link to="/" className="block px-4 py-2 text-sm text-gray-700">
                         로그아웃
                       </Link>
                     </Menu.Item>
