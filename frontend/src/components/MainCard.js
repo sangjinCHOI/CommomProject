95f5d90f85561d9f685488a9d1ff49dc22ddc499
DESKTOP-TMRS7FM\pkonu <pjhq2@naver.com>
2022-01-26 12:15:48 +0900

    feat: MainBox height 변수 처리

--- frontend/src/components/MainCard.js
+++ frontend/src/components/MainCard.js
@@ -1,6 +1,6 @@
-export default function MainCard({ children }) {
+export default function MainCard({ children, height = "400px" }) {
   return (
-    <div className="bg-white w-100 rounded-lg border mb-4 p-4" style={{ height: "800px" }}>
+    <div className="bg-white w-100 rounded border-2 mb-4 p-4" style={{ height: height }}>
       {children}
     </div>
   );
--- frontend/src/router/Home.js
+++ frontend/src/router/Home.js
@@ -3,7 +3,10 @@ import MainCard from "../components/MainCard";
 export default function Home() {
   return (
     <div className="text-3xl">
-      <MainCard>
+      <MainCard height="200px">
+        this is chilasdasdasda<div>asdasd</div>
+      </MainCard>
+      <MainCard height="700px">
         this is chilasdasdasda<div>asdasd</div>
       </MainCard>
     </div>
