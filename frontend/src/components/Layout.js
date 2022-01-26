2e6d3c33f23c93060b681e264f1e7bac099438e9
최상진 <toramx2@naver.com>
2022-01-26 11:38:42 +0900

    Design: Layout 수정

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,14 +1,11 @@
 import Navbar from "./Navbar";
-import SettingSideBar from "./SettingSideBar";
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <main className="grid grid-cols-4 gap-x-4">
-        <div className="col-span-1"></div>
-        <div className="col-span-2">{children}</div>
-        <div className="col-span-1"></div>
+      <main className="flex justify-center">
+        <div className="max-w-screen-sm w-full">{children}</div>
       </main>
     </>
   );
