a41fdd291cbfd2e23589fa9e9a008ae3e01349db
pangE <cdpkct@gmail.com>
2022-01-27 10:55:20 +0900

    Fix: add RequestBody in singup function

--- backend/src/main/java/com/ssafy/persona/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/persona/user/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,7 +56,7 @@ public class UserController {
 	}
 	
 	@PostMapping
-	public ResponseEntity signupUser(UserSignupRequest user) {
+	public ResponseEntity signupUser(@RequestBody UserSignupRequest user) {
 		if(userService.userSignup(user.toUser()) > 0) {
 			
 			Mail mail = new Mail();
