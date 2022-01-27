82b9c841a949450e45efc4b20ec49e49e3a3c9b1
최상진 <toramx2@naver.com>
2022-01-27 17:55:03 +0900

    Fix: cotent 모달 클릭 메뉴 수정

--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -11,7 +11,7 @@ export default function Content() {
           <div className="text-xl">
             <p>초밥왕김탁구</p>
           </div>
-          <Menu as="div" className="mx-2">
+          <Menu as="div" className="mx-2 relative">
             <Menu.Button className="flex text-sm">
               <span className="material-icons">more_horiz</span>
             </Menu.Button>
@@ -48,7 +48,7 @@ export default function Content() {
             </div>
           </div>
           <div className="flex items-center">
-            <Menu as="div" className="mx-2">
+            <Menu as="div" className="mx-2 relative">
               <Menu.Button className="flex text-sm">
                 <span className="material-icons">library_add_check</span>
               </Menu.Button>
