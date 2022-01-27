2c7ac52da5ec9835175f3e1a9b168c8f70abcc52
DESKTOP-TMRS7FM\pkonu <pjhq2@naver.com>
2022-01-27 11:46:40 +0900

    feat: MainCard.js class 변수로 받는 기능 추가

--- frontend/src/components/MainCard.js
+++ frontend/src/components/MainCard.js
@@ -1,6 +1,9 @@
-export default function MainCard({ children, height }) {
+export default function MainCard({ children, height, border, margin, padding, rounded }) {
   return (
-    <div className="bg-white w-100" style={{ height: height }}>
+    <div
+      className={`bg-white w-100 ${border} ${margin} ${padding} ${rounded}`}
+      style={{ height: height }}
+    >
       {children}
     </div>
   );
