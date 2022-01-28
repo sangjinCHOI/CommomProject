9d57250dc83873c3f1c4e28c3f485e3cc3ebb58c
최대치 <eocl777@naver.com>
2022-01-28 15:03:37 +0900

    Design : 주소 수정

--- frontend/src/store/userStore.js
+++ frontend/src/store/userStore.js
@@ -4,8 +4,8 @@ export default createStore(function (state, action) {
   if (state === undefined) {
     return { iddata: "test" };
   }
-  if (action.type === "idInquiry") {
-    return { data: action.data };
+  if (action.type === "idtrans") {
+    return { iddata: action.iddata };
   }
   return state;
 }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
