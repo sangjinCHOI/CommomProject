5930d6f98ee9d3100aa65396f7bebd307fe981db
DESKTOP-TMRS7FM\pkonu <pjhq2@naver.com>
2022-02-03 14:04:22 +0900

    refactor: charactersStore 수정 시작 전 커밋

--- frontend/src/store/charactersStore.js
+++ frontend/src/store/charactersStore.js
@@ -1,11 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 // 나중에는 서버에서 캐릭터 개수에 따라 각자 적절히 받아와야 할 듯?
+// 캐릭터 전체 리스트로 변환 필요
 const initialCharactersState = {
   nickname: "initialNickname",
   categorySeq: 0,
   introduction: "initialIntroduction",
+  // alarmAllow: false,
+  // characterActive: true,
+  // characterCreatedDate: null,
+  // characterModifiedDate: null,
+  // characterSeq: 0,
+  // followAlarm: false,
+  // modifyAlarm: false,
+  // replyAlarm: false,
+  // reportedTime: 0,
+  // representativeAchievement: 0
 };
+
+// const initialCharactersState = []
 
 const charactersSlice = createSlice({
   name: "character",
