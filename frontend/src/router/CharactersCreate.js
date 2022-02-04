e909979ab14b40e4bcb2ec5709231139b4550213
DESKTOP-TMRS7FM\pkonu <pjhq2@naver.com>
2022-02-04 19:48:16 +0900

    style: CharactersCreate 주석 수정

--- frontend/src/router/CharactersCreate.js
+++ frontend/src/router/CharactersCreate.js
@@ -35,7 +35,8 @@ function CharactersCreate({ characterSlice, saveCharacter, location }) {
 
   const { userSeq, userId } = location.state;
 
-  // 현재 캐릭터 삭제 -> 캐릭터 0개일 때 캐릭터 생성 -> 캐릭터 선택 창에서 새로고침 할 때 캐릭터 못불러오는 버그 존재 --> 로컬스토리지+토큰??
+  // 현재 캐릭터 생성 -> 캐릭터 선택 창에서 새로고침 할 때 캐릭터 못불러오는 버그 존재 -> props로 userId/userSeq를 전달하면 새로고침 하면 사라지기 때문에
+  // 결국 userStore를 새로 만드는 방법 뿐인가?
   const characterSave = (e) => {
     e.preventDefault();
     const data = {
