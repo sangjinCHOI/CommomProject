3394cf973761103350127ee5b4e622801df4fbf5
kimdy96 <kp03@naver.com>
2022-02-07 13:29:11 +0900

    fix: modify contentcontroller

--- backend/src/main/java/com/ssafy/persona/content/controller/ContentController.java
+++ backend/src/main/java/com/ssafy/persona/content/controller/ContentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,6 +37,7 @@ import io.swagger.annotations.ApiParam;
 
 @RestController
 @Api("content controller API")
+@CrossOrigin(origins = {"*"}, maxAge = 6000)
 public class ContentController {
 	private static final String SUCCESS = "success";
 	private static final String FAIL = "fail";
