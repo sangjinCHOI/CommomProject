a6de57d96a0a0991c19c13a0b15cbdbb5c189b6a
최상진 <toramx2@naver.com>
2022-02-08 15:20:25 +0900

    Feat: comment 작성, 읽기

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Menu } from "@headlessui/react";
 import { Image, Modal, ModalHeader, ModalBody } from "@material-tailwind/react";
 import ReactReadMoreReadLess from "react-read-more-read-less";
 import Report from "../components/Report";
 
-export default function Comment(props) {
+function Comment(props) {
   const { isOpen, onCancel } = props;
   const handleCommentClose = () => {
     onCancel();
@@ -22,41 +23,48 @@ export default function Comment(props) {
         </ModalHeader>
         <hr className="mb-5" />
         <ModalBody>
-          <div style={{ overflowY: "scroll", width: 500, height: 320 }}>
-            <div className="mb-5">
-              <div className="px-4 flex justify-between">
-                <div className="flex">
-                  <Image src="https://url.kr/p6xvn5" width="32px" rounded={true} raised={false} alt="Rounded Image" />
-                  <p className="pt-0.5 mx-2 text-lg">초밥왕김탁구</p>
-                </div>
-                <Menu as="div" className="mx-2 mt-1 relative w-12">
-                  <Menu.Button className="flex text-sm">
-                    <span className="material-icons">more_horiz</span>
-                  </Menu.Button>
-                  <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white flex flex-col border-2">
-                    <Menu.Item>
-                      <button className="mx-4 text-sm" onClick={() => setReportModal(true)}>
-                        댓글 신고
-                      </button>
-                    </Menu.Item>
-                    <Menu.Item>
-                      <button className="mx-4 text-sm">삭제</button>
-                    </Menu.Item>
-                  </Menu.Items>
-                </Menu>
-              </div>
-              <div style={{ width: 400, paddingLeft: 57 }}>
-                <ReactReadMoreReadLess
-                  charLimit={50}
-                  readMoreText={"더보기"}
-                  readLessText={"숨기기"}
-                  readMoreStyle={{ color: "gray" }}
-                  readLessStyle={{ color: "gray" }}
-                >
-                  동해물과백두산이마르고닳도록하느님이보우하사우리나라만세무궁화삼천리화려강산대한사람대한으로길이보전하세동해물과백두산이마르고닳도록하느님이보우하사우리나라만세무궁화삼천리화려강산대한사람대한으로길이보전하세
-                </ReactReadMoreReadLess>
-              </div>
-            </div>
+          <div style={{ overflowY: "scroll", width: 500, height: 350 }}>
+            {props.comments
+              ? props.comments.map((comment, index) => {
+                  return (
+                    <div className="mb-5" key={index}>
+                      <div className="px-4 flex justify-between">
+                        <div className="flex">
+                          <Image src="https://url.kr/p6xvn5" width="32px" rounded={true} raised={false} alt="Rounded Image" />
+                          <p className="pt-0.5 mx-2 text-lg">초밥왕김탁구</p>
+                        </div>
+                        <Menu as="div" className="mx-2 mt-1 relative w-12">
+                          <Menu.Button className="flex text-sm">
+                            <span className="material-icons">more_horiz</span>
+                          </Menu.Button>
+                          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white flex flex-col border-2">
+                            <Menu.Item>
+                              <button className="mx-4 text-sm" onClick={() => setReportModal(true)}>
+                                댓글 신고
+                              </button>
+                            </Menu.Item>
+                            <Menu.Item>
+                              <button className="mx-4 text-sm">삭제</button>
+                            </Menu.Item>
+                          </Menu.Items>
+                        </Menu>
+                      </div>
+                      <div style={{ width: 400, paddingLeft: 57 }}>
+                        <ReactReadMoreReadLess
+                          key={index}
+                          charLimit={50}
+                          readMoreText={"더보기"}
+                          readLessText={"숨기기"}
+                          readMoreStyle={{ color: "gray" }}
+                          readLessStyle={{ color: "gray" }}
+                        >
+                          {comment.replyText}
+                        </ReactReadMoreReadLess>
+                      </div>
+                    </div>
+                  );
+                })
+              : null}
           </div>
         </ModalBody>
       </Modal>
@@ -64,3 +72,9 @@ export default function Comment(props) {
     </>
   );
 }
+
+function mapStateToProps(state) {
+  return { characterSlice: state.character };
+}
+
+export default connect(mapStateToProps)(Comment);
