226659c85d2032a51b8ad3201864c5d6e0d549e5
pangE <cdpkct@gmail.com>
2022-02-09 15:40:37 +0900

    Feat: make file custom axios

---
+++ frontend/src/config/File.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:8082",
+  headers: {
+    "Content-Type": "multipart/form-data",
+    token: localStorage.getItem("idToken"),
+  },
+});
+
+instance.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
