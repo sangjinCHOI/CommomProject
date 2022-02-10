76a73fb6763d9d7687598557d3d3a7cef2f82d7d
pangE <cdpkct@gmail.com>
2022-02-10 18:08:51 +0900

    Refactor: delete comment line

--- frontend/src/router/Storages.js
+++ frontend/src/router/Storages.js
@@ -35,15 +35,12 @@ function SearchCharacters({ location, characterSlice }) {
     <div>
       <div className="my-4">'{nickname}'의 저장소</div>
       <MainCard classes="border rounded py-2">
-        {/* <StorageCardLarge storageName="요리모음 저장목록" imgSrc="https://t1.daumcdn.net/cfile/tistory/99E3B54C5C1DEB9C38" /> */}
         <div>
           {contentArr.map((content) => (
             <StorageCardLarge key={content.storageSeq} storageName={content.storageName} imgSrc={content.storageImagePath} {..."/"} {...content.storageImageName}></StorageCardLarge>
             //            <div key={content.storageSeq}>{content.storageName}</div>
           ))}
         </div>
-        {/* <StorageCardLarge storageName="코딩 저장목록" imgSrc="http://media.fastcampus.co.kr/wp-content/uploads/2021/03/fastcampus-media-coding-img-1-1030x539.png" />
-        <StorageCardLarge storageName="고양이 저장목록" imgSrc="https://cdn2.thecatapi.com/images/cna.jpg" /> */}
       </MainCard>
     </div>
   );
