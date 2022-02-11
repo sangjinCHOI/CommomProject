27a020d2c84e6e4053bab8702bd843a400455b76
DESKTOP-TMRS7FM\pkonu <pjhq2@naver.com>
2022-02-12 01:24:34 +0900

    feat: ErrorBoundary page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,58 +32,62 @@ import SearchTexts from "./router/SearchTexts";
 import Storages from "./router/Storages";
 import StoragesDetail from "./router/StoragesDetail";
 
+import ErrorBoundary from "./components/ErrorBoundary";
+
 function App() {
   return (
     <>
-      <Router>
-        <Switch>
-          {/* Layout 필요 없는 주소 */}
-          <Route exact path="/accounts/signup" component={Signup} />
-          <Route path="/accounts/signup/email" component={SignupEmail} />
-          <Route path="/accounts/login" component={Login} />
-          <Route exact path="/accounts/id_inquiry" component={IdInquiry} />
-          <Route path="/accounts/id_inquiry/result" component={IdInquiryResult} />
-          <Route exact path="/accounts/pw_inquiry" component={PwInquiry} />
-          <Route exact path="/accounts/pw_inquiry/result" component={PwInquiryResult} />
-          <Route exact path="/characters/select" component={Characters} />
-          <Route exact path="/characters/create" component={CharactersCreate} />
-          <Route exact path="/characters/update" component={CharactersUpdate} />
+      <ErrorBoundary>
+        <Router>
+          <Switch>
+            {/* Layout 필요 없는 주소 */}
+            <Route exact path="/accounts/signup" component={Signup} />
+            <Route path="/accounts/signup/email" component={SignupEmail} />
+            <Route path="/accounts/login" component={Login} />
+            <Route exact path="/accounts/id_inquiry" component={IdInquiry} />
+            <Route path="/accounts/id_inquiry/result" component={IdInquiryResult} />
+            <Route exact path="/accounts/pw_inquiry" component={PwInquiry} />
+            <Route exact path="/accounts/pw_inquiry/result" component={PwInquiryResult} />
+            <Route exact path="/characters/select" component={Characters} />
+            <Route exact path="/characters/create" component={CharactersCreate} />
+            <Route exact path="/characters/update" component={CharactersUpdate} />
 
-          {/* SettingLayout 필요한 주소 */}
-          <Route path="/settings/:path?" exact>
-            <SettingLayout>
-              <Switch>
-                <Route exact path="/settings/character" component={SettingsCharacter} />
-                <Route exact path="/settings/account" component={SettingsAccount} />
-                <Route exact path="/settings/alarm" component={SettingsAlarm} />
-                <Route exact path="/settings/help" component={SettingsHelp} />
-              </Switch>
-            </SettingLayout>
-          </Route>
+            {/* SettingLayout 필요한 주소 */}
+            <Route path="/settings/:path?" exact>
+              <SettingLayout>
+                <Switch>
+                  <Route exact path="/settings/character" component={SettingsCharacter} />
+                  <Route exact path="/settings/account" component={SettingsAccount} />
+                  <Route exact path="/settings/alarm" component={SettingsAlarm} />
+                  <Route exact path="/settings/help" component={SettingsHelp} />
+                </Switch>
+              </SettingLayout>
+            </Route>
 
-          {/* Layout 필요한 주소 */}
-          <Route>
-            <Layout>
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/alarm/center" component={Alarm} />
-                <Route exact path="/search" component={Search} />
-                <Route exact path="/search/characters" component={SearchCharacters} />
-                <Route exact path="/search/storages" component={SearchStorages} />
-                <Route exact path="/search/tags" component={SearchTags} />
-                <Route exact path="/search/tag" component={SearchTagsDetail} />
-                <Route exact path="/search/texts" component={SearchTexts} />
-                {/* characters, login, signup같은 닉네임이 있다면 문제 발생 가능 주의 */}
-                <Route exact path="/:nickname" component={Profile} />
-                <Route exact path="/:nickname/follow" component={Follow} />
-                <Route exact path="/:nickname/achievement" component={Achievement} />
-                <Route exact path="/:nickname/storages" component={Storages} />
-                <Route exact path="/:nickname/storages/:storage_seq" component={StoragesDetail} />
-              </Switch>
-            </Layout>
-          </Route>
-        </Switch>
-      </Router>
+            {/* Layout 필요한 주소 */}
+            <Route>
+              <Layout>
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/alarm/center" component={Alarm} />
+                  <Route exact path="/search" component={Search} />
+                  <Route exact path="/search/characters" component={SearchCharacters} />
+                  <Route exact path="/search/storages" component={SearchStorages} />
+                  <Route exact path="/search/tags" component={SearchTags} />
+                  <Route exact path="/search/tag" component={SearchTagsDetail} />
+                  <Route exact path="/search/texts" component={SearchTexts} />
+                  {/* characters, login, signup같은 닉네임이 있다면 문제 발생 가능 주의 */}
+                  <Route exact path="/:nickname" component={Profile} />
+                  <Route exact path="/:nickname/follow" component={Follow} />
+                  <Route exact path="/:nickname/achievement" component={Achievement} />
+                  <Route exact path="/:nickname/storages" component={Storages} />
+                  <Route exact path="/:nickname/storages/:storage_seq" component={StoragesDetail} />
+                </Switch>
+              </Layout>
+            </Route>
+          </Switch>
+        </Router>
+      </ErrorBoundary>
     </>
   );
 }
