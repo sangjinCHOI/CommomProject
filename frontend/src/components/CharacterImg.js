d8d454ad530083c82bff2dd56d3188dd4a28ea52
DESKTOP-TMRS7FM\pkonu <pjhq2@naver.com>
2022-02-11 18:43:43 +0900

    fix: previewImage.src에 require 추가

--- frontend/src/components/CharacterImg.js
+++ frontend/src/components/CharacterImg.js
@@ -12,7 +12,6 @@ export default function CharacterImg({
   imgChangeHandler,
 }) {
   const [isClick, setIsClick] = useState(false);
-
   console.log("3번", imgSrc);
 
   const resetImage = () => {
@@ -20,7 +19,8 @@ export default function CharacterImg({
     // 기본 이미지도 서버에 저장해야함!(require 이슈) -> 아닐수도
     // previewImage.src = "https://cdn2.thecatapi.com/images/9gg.jpg";
     // previewImage.src = defaultUserImg; // 이건 겉보기
-    previewImage.src = "/images/default_user.png"; // 서버일 때 기본 이미지 주소
+    // previewImage.src = "/images/default_user.png"; // 서버일 때 기본 이미지 주소
+    previewImage.src = require(`../assets/images/default_user.png`);
 
     //// is_active = 0으로 만드는 API 호출 추가 예정
     // 부모 컴포넌트의 함수를 통해 이미지 전달(뺄수도?)
