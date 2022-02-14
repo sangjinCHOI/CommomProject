9b4445d6e8d817c975da96238fda954c36a8461a
pangE <cdpkct@gmail.com>
2022-02-14 10:43:38 +0900

    Test: test code

--- frontend/src/components/StorageCardLarge.js
+++ frontend/src/components/StorageCardLarge.js
@@ -4,6 +4,7 @@ import styles from "./StorageCardLarge.module.css";
 export default function StorageCardLarge({ storageName, imgSrc }) {
   const [isMouseOver, setIsMouseOver] = useState(false);
   console.log(imgSrc);
+  if (imgSrc == 0) imgSrc = "/images/save_box.jpg";
   return (
     <div className={`relative flex justify-center items-center px-4 py-2`} style={{ height: "200px" }} onMouseOver={() => setIsMouseOver(true)} onMouseOut={() => setIsMouseOver(false)}>
       <img
