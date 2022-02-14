7eae217d77c7635ff5c68a9588d29c5ac09f84c8
pangE <cdpkct@gmail.com>
2022-02-15 02:00:27 +0900

    Feat: achievement add

--- frontend/src/components/AchievementContent.js
+++ frontend/src/components/AchievementContent.js
@@ -1,72 +1,98 @@
 import React from "react";
+import { useState } from "react";
 import { Menu } from "@headlessui/react";
 import { Label } from "@material-tailwind/react";
 import MainCard from "./MainCard";
 import ExAchievment from "../assets/images/Achievement01.png";
+import { connect } from "react-redux";
 import styles from "../components/Achievement.module.css";
 import H5 from "@material-tailwind/react/Heading5";
 import H6 from "@material-tailwind/react/Heading6";
 import trophy from "../assets/images/trophy.png";
+import Send from "../config/Send";
 
-export default function Content() {
+function Content({ characterSlice }) {
+  const [achievements, setAchievements] = useState([]);
+
+  const data = {
+    achievementType: 1,
+    characterSeq: characterSlice.characterSeq,
+    level: 0,
+  };
+  Send.post(`/character/acievements`, JSON.stringify(data))
+    .then((res) => {
+      if (res.status == 200) setAchievements(res.data);
+      else alert("error!!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   return (
     <>
-      <MainCard max-height="900px">
-        <div style={{ height: 180 }} className="p-4 flex justify-between">
-          <div className="flex justify-center">
-            <div className="flex justify-center" style={{ width: 565 }}>
-              <div className="flex justify-start " style={{ width: 380 }}>
-                <div style={{ textAlign: "left", width: 360 }}>
-                  <H5>[우리는 깐부잖아]</H5>
-                  <p>다른 사람 게시물 저장 시 달성</p>
-                  <br />
-                  <div className="flex justify-start">
-                    <img src={trophy} style={{ width: 73, height: 73 }} />
-                    <div>
-                      <H5>Achieve!!</H5>
-                      <H6>2022-01-17</H6>
+      <div>
+        {achievements.map((achieve) => (
+          <div>
+            {achieve.isgained ? (
+              <MainCard max-height="900px">
+                <div style={{ height: 180 }} className="p-4 flex justify-between">
+                  <div className="flex justify-center">
+                    <div className="flex justify-center" style={{ width: 565 }}>
+                      <div className="flex justify-start " style={{ width: 380 }}>
+                        <div style={{ textAlign: "left", width: 360 }}>
+                          {/* <H5>[우리는 깐부잖아]</H5> */}
+                          <H5>[{achieve.achievementName}]</H5>
+                          <p>{achieve.achievementCondition}</p>
+                          <br />
+                          <div className="flex justify-start">
+                            <img src={trophy} style={{ width: 73, height: 73 }} />
+                            <div>
+                              <H5>Achieve!!</H5>
+                              <H6>{achieve.achievementCreatedDate}</H6>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex justify-end bg-slate-100">
+                        <img style={{ maxWidth: 150, maxHeight: 150, objectFit: "cover" }} src={ExAchievment} />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="flex justify-end bg-slate-100">
-                <img style={{ maxWidth: 150, maxHeight: 150, objectFit: "cover" }} src={ExAchievment} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </MainCard>
-      {/* ****************************************************************************** */}
-      <MainCard max-height="900px">
-        <div style={{ height: 180 }} className="p-4 flex justify-between bg-slate-100">
-          <div className="flex justify-center ">
-            <div className="flex justify-center" style={{ width: 565 }}>
-              <div className="flex justify-start " style={{ width: 380 }}>
-                <div style={{ textAlign: "left", width: 360 }}>
-                  <H5>[우리는 깐부잖아]</H5>
-                  {/* <p>다른 사람 게시물 저장 시 달성</p> */}
-                  <p>깐부끼리는 니꺼 내꺼가 없는거야</p>
-                  <br />
-                  <div className="flex justify-start">
-                    <img src={trophy} style={{ width: 73, height: 73 }} />
-                    {/* <div>
-                      <H5>Achieve!!</H5>
-                      <H6>2022-01-17</H6>
-                    </div> */}
+              </MainCard>
+            ) : (
+              <MainCard max-height="900px">
+                <div style={{ height: 180 }} className="p-4 flex justify-between bg-slate-100">
+                  <div className="flex justify-center ">
+                    <div className="flex justify-center" style={{ width: 565 }}>
+                      <div className="flex justify-start " style={{ width: 380 }}>
+                        <div style={{ textAlign: "left", width: 360 }}>
+                          <H5>[{achieve.achievementName}]</H5>
+                          <p>{achieve.achievementDescription}</p>
+                          <br />
+                          <div className="flex justify-start">
+                            <img src={trophy} style={{ width: 73, height: 73 }} />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex justify-end bg-slate-100">
+                        <img className={`${styles.gray}`} style={{ maxWidth: 150, maxHeight: 150, objectFit: "cover" }} src={ExAchievment} />
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="flex justify-end bg-slate-100">
-                <img
-                  //className={`${styles.gray}`}
-                  style={{ maxWidth: 150, maxHeight: 150, objectFit: "cover" }}
-                  src={ExAchievment}
-                />
-              </div>
-            </div>
+              </MainCard>
+            )}
           </div>
-        </div>
-      </MainCard>
+        ))}
+      </div>
+      {/* ****************************************************************************** */}
     </>
   );
 }
+
+function mapStateToProps(state) {
+  return { characterSlice: state.character };
+}
+
+export default connect(mapStateToProps)(Content);
