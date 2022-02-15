47936aeec55234884e60497c60041898d1d5ab79
pangE <cdpkct@gmail.com>
2022-02-16 04:04:32 +0900

    Feat: add JWT config

--- backend/src/main/java/com/ssafy/persona/interceptor/GeneralInterceptor.java
+++ backend/src/main/java/com/ssafy/persona/interceptor/GeneralInterceptor.java
@@ -2,13 +2,17 @@ package com.ssafy.persona.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ssafy.persona.domain.user.security.SecurityService;
+
+import java.util.Map;
 
 @Component
 public class GeneralInterceptor implements HandlerInterceptor{
@@ -19,13 +23,26 @@ public class GeneralInterceptor implements HandlerInterceptor{
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		//System.out.println(request.getHeader("token"));
-		
+		HttpSession session = request.getSession();
+
+		System.out.println("this");
+		System.out.println(request.getHeader("token"));
+		//System.out.println(securityService.getSubject(request.getHeader("token")));
+		//System.out.println(session.getAttribute("token"));
+		Map<String, Object> map = null;
+
 
 		try {
+			//System.out.println(securityService.getSubject(session.getAttribute("token").toString()));
 			//securityService.getSubject(session.getAttribute("token").toString());
-			
+			// JSON 파일을 MAP으로 변환
+			map = new ObjectMapper().readValue(request.getHeader("token").toString(), Map.class);
+			securityService.getSubject(map.get("token").toString());
+//			System.out.println(securityService.getSubject(map.get("token").toString()));
+//			System.out.println(map.get("token"));
+
 		} catch (Exception e) {
+			System.out.println("허가받지않은 사용자");
 			// 로그인 페이지로 redirect 해야함
 			// 사용자 정의 Exception으로 throw 하기
 			//throw new myException
