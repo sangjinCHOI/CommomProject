a99e1b62bd0ed27b154741abbafc8905c275ea30
pangE <cdpkct@gmail.com>
2022-02-16 13:22:57 +0900

    Test: test code

--- frontend/src/components/CharacterProfile.js
+++ frontend/src/components/CharacterProfile.js
@@ -84,8 +84,10 @@ function CharacterProfile({
         <div>
           <div className="inline-block bg-red-500 px-1 mr-1 rounded-xl">
             <img
-              //src={characterSlice.representativeAchievement ? require(`../assets/achievements/${characterSlice.representativeAchievement}.png`) : require("../assets/images/sample_achievement.png")}
-              src={require("../assets/images/sample_achievement.png")}
+              src={
+                characterSlice.representativeAchievement != 0 ? require(`../assets/achievements/${characterSlice.representativeAchievement}.png`) : require("../assets/images/sample_achievement.png")
+              }
+              //src={require("../assets/images/sample_achievement.png")}
               alt="sample_achievement_img"
               width="16px"
               className="inline-block"
