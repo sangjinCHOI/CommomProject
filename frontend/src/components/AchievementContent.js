d9df291a5063c08376c8d19a2d6b154f3b988415
pangE <cdpkct@gmail.com>
2022-02-16 11:36:06 +0900

    Test: test code

--- frontend/src/components/AchievementContent.js
+++ frontend/src/components/AchievementContent.js
@@ -77,15 +77,15 @@ function Content({ characterSlice, achievements }) {
                           style={{ maxWidth: 150, maxHeight: 150, objectFit: "cover" }}
                           src={require(`../assets${achieve.filePath + achieve.fileName}`)}
                           alt={require(`../assets${achieve.filePath + achieve.fileName}`)}
-                          className={isRepresentative ? "border-4 border-yellow-400 rounded-lg" : "border-4 border-gray-400 rounded-lg"}
+                          className={isRepresentative == achieve.achievementSeq ? "border-4 border-yellow-400 rounded-lg" : "border-4 border-gray-400 rounded-lg"}
                         />
                         {/* isRepresentative로 임시로 대표 업적에 따라 색깔 바뀌개 해놨습니다. */}
                         <div
-                          onClick={isRepresentative ? deleteAchievement() : updateAchievement(achieve.achievementSeq)}
+                          onClick={isRepresentative == achieve.achievementSeq ? deleteAchievement() : updateAchievement(achieve.achievementSeq)}
                           className={`absolute right-7 px-4 py-0.5 rounded-lg font-semibold ${isRepresentative ? "bg-gray-200 text-gray-400" : "bg-orange-200 text-orange-500"}`}
                           style={{ cursor: "pointer", top: "155px" }}
                         >
-                          {isRepresentative ? "대표 해제" : "대표 설정"}
+                          {isRepresentative == achieve.achievementSeq ? "대표 해제" : "대표 설정"}
                         </div>
                       </div>
                     </div>
