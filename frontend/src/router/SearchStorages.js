c50be8eb6950f87faf982ad08b00e2573b153b63
DESKTOP-TMRS7FM\pkonu <pjhq2@naver.com>
2022-02-16 09:52:45 +0900

    feat: 저장소 검색 결과

--- frontend/src/router/SearchStorages.js
+++ frontend/src/router/SearchStorages.js
@@ -1,16 +1,29 @@
+import { connect } from "react-redux";
 import MainCard from "../components/MainCard";
 import StorageCardLarge from "../components/StorageCardLarge";
 
-export default function SearchCharacters({ location }) {
+function SearchCharacters({ location, characterSlice }) {
   const queryString = location.search;
   const params = new URLSearchParams(queryString);
   const query = params.get("query");
+  const { storagesResultList } = location.props;
 
   return (
     <div>
       <div className="my-4">'{query}' 저장소 검색 결과</div>
       <MainCard classes="border rounded-xl py-5">
-        <StorageCardLarge
+        {storagesResultList.map((storage) => (
+          // 현재 nickname -> ninkname인 상태
+          <StorageCardLarge
+            key={storage.storageSeq}
+            storageName={storage.storageSeq}
+            imgSrc={storage.filePath + storage.fileName}
+            ownerNickname={storage.ninkname}
+            characterSeq={characterSlice.characterSeq}
+            storageSeq={storage.storageSeq}
+          />
+        ))}
+        {/* <StorageCardLarge
           storageName="요리하는 부부 저장소"
           imgSrc="https://cdn2.thecatapi.com/images/43n.png"
         />
@@ -21,8 +34,14 @@ export default function SearchCharacters({ location }) {
         <StorageCardLarge
           storageName="불타는 요리 맛집"
           imgSrc="https://cdn2.thecatapi.com/images/cna.jpg"
-        />
+        /> */}
       </MainCard>
     </div>
   );
 }
+
+function mapStateToProps(state) {
+  return { characterSlice: state.character };
+}
+
+export default connect(mapStateToProps)(SearchCharacters);
