c42e01d66fc8eb082dd29ae3aa1e146c374cc9b2
pangE <cdpkct@gmail.com>
2022-02-16 11:56:06 +0900

    Test: rollback code

--- frontend/src/components/CharacterProfile.js
+++ frontend/src/components/CharacterProfile.js
@@ -82,7 +82,8 @@ function CharacterProfile({
         <div>
           <div className="inline-block bg-red-500 px-1 mr-1 rounded-xl">
             <img
-              src={characterSlice.representativeAchievement ? require(`../assets/achievements/${characterSlice.representativeAchievement}.png`) : require("../assets/images/sample_achievement.png")}
+              //src={characterSlice.representativeAchievement ? require(`../assets/achievements/${characterSlice.representativeAchievement}.png`) : require("../assets/images/sample_achievement.png")}
+              src={require("../assets/images/sample_achievement.png")}
               alt="sample_achievement_img"
               width="16px"
               className="inline-block"
