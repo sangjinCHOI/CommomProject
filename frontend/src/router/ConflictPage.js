a6c8bee85b3852ea3b9e5349d02dfa4ea60d0004
pangE <cdpkct@gmail.com>
2022-02-17 11:32:56 +0900

    Feat: add error page

--- frontend/src/router/ConflictPage.js
+++ frontend/src/router/ConflictPage.js
@@ -1,19 +1,19 @@
 import MainCard from "../components/MainCard";
 import { useHistory } from "react-router-dom";
 
-export default function NotFound() {
+export default function ConflictPage() {
   let history = useHistory();
   return (
     <MainCard classes="rounded-xl h-80 mt-12">
       <div className="flex justify-center items-center">
         <div className="text-center mt-14" style={{ width: "250px" }}>
-          <div style={{ fontSize: "80px" }}>404</div>
-          <div style={{ fontSize: "20px" }}>NOT FOUND</div>
+          <div style={{ fontSize: "80px" }}>409</div>
+          <div style={{ fontSize: "20px" }}>Conflict</div>
         </div>
 
         <div className="text-center mt-16" style={{ width: "250px" }}>
           <img src={require("../assets/images/main_logo.png")} alt="main_logo" style={{ width: "120px" }} className="mt-10 mb-2 ml-8" />
-          <p>페이지를 찾을 수 없습니다.</p>
+          <p>이미 인증된 사용자입니다.</p>
         </div>
       </div>
       <div onClick={() => history.push("/")} className="flex justify-center mt-10" style={{ cursor: "pointer" }}>
--- frontend/src/router/ForbiddenPage.js
+++ frontend/src/router/ForbiddenPage.js
@@ -1,19 +1,19 @@
 import MainCard from "../components/MainCard";
 import { useHistory } from "react-router-dom";
 
-export default function NotFound() {
+export default function ForbiddenPage() {
   let history = useHistory();
   return (
     <MainCard classes="rounded-xl h-80 mt-12">
       <div className="flex justify-center items-center">
         <div className="text-center mt-14" style={{ width: "250px" }}>
-          <div style={{ fontSize: "80px" }}>404</div>
-          <div style={{ fontSize: "20px" }}>NOT FOUND</div>
+          <div style={{ fontSize: "80px" }}>403</div>
+          <div style={{ fontSize: "20px" }}>Forbidden</div>
         </div>
 
         <div className="text-center mt-16" style={{ width: "250px" }}>
           <img src={require("../assets/images/main_logo.png")} alt="main_logo" style={{ width: "120px" }} className="mt-10 mb-2 ml-8" />
-          <p>페이지를 찾을 수 없습니다.</p>
+          <p>해당 페이지에 접속할 권한이 없습니다.</p>
         </div>
       </div>
       <div onClick={() => history.push("/")} className="flex justify-center mt-10" style={{ cursor: "pointer" }}>
